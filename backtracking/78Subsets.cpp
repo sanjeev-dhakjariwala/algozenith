c695fae4b2106ef979c9f7e11015a3bad37f38ac
sanjeev-dhakjariwala <sanjeevjha882@gmail.com>
2025-03-05 23:07:09 +0530

    03-MARCH-2025

--- backtracking/78Subsets.cpp
+++ backtracking/78Subsets.cpp
@@ -1,34 +1,67 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Solution {
-    public:
-        vector<vector<int>> ans;
-        int n;
-        void rec(int level, vector<int>& nums, vector<int>& temp){
-            if(level == n){
-                ans.push_back(temp);
-                return;
-            }
+/* class Solution
+{
+public:
+    vector<vector<int>> ans;
+    int n;
+    void rec(int level, vector<int> &nums, vector<int> &temp)
+    {
+        if (level == n)
+        {
             ans.push_back(temp);
-            for(int i = level; i < n; i++){
-                temp.push_back(nums[i]);
-                rec(i + 1, nums, temp);
-                temp.pop_back();
-            }
+            return;
         }
-        vector<vector<int>> subsets(vector<int>& nums) {
-            n = nums.size();
-            vector<int> temp;
-            rec(0, nums, temp);
-            return ans;
+        ans.push_back(temp);
+        for (int i = level; i < n; i++)
+        {
+            temp.push_back(nums[i]);
+            rec(i + 1, nums, temp);
+            temp.pop_back();
         }
-    };
-int main(){
+    }
+    vector<vector<int>> subsets(vector<int> &nums)
+    {
+        n = nums.size();
+        vector<int> temp;
+        rec(0, nums, temp);
+        return ans;
+    }
+}; */
+class Solution
+{
+public:
+    vector<vector<int>> ans;
+    int n;
+    void rec(int level, vector<int> &nums, vector<int> &temp)
+    {
+        if (level == n)
+        {
+            ans.push_back(temp);
+            return;
+        }
+        temp.push_back(nums[level]);
+        rec(level + 1, nums, temp);
+        temp.pop_back();
+        rec(level + 1, nums, temp);
+    }
+    vector<vector<int>> subsets(vector<int> &nums)
+    {
+        n = nums.size();
+        vector<int> temp;
+        rec(0, nums, temp);
+        return ans;
+    }
+};
+int main()
+{
     Solution obj;
     vector<int> nums = {1, 2, 3};
     vector<vector<int>> ans = obj.subsets(nums);
-    for(auto x: ans){
-        for(auto y: x){
+    for (auto x : ans)
+    {
+        for (auto y : x)
+        {
             cout << y << " ";
         }
         cout << endl;
